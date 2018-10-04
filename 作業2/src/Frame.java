0bb263014bd179175c55de50bf59e9f05e1009de
zxcvbnm199905040213 <32055977+zxcvbnm199905040213@users.noreply.github.com>
2018-10-04 10:12:54 +0800

    Update Frame.java

--- 作業2/src/Frame.java
+++ 作業2/src/Frame.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Frame extends JFrame{
+    private JMenuBar jmb = new JMenuBar();
+    private JMenu jmnuFile = new JMenu("File");
+    private JMenu jmnuTool = new JMenu("Tool");
+    private JMenu jmnuGame = new JMenu("Game");
+    private JMenu jmnuHelp = new JMenu("Help");
+    private JMenuItem jmiOpen = new JMenuItem("Open");
+    private JMenuItem jmiClose = new JMenuItem("Close");
+    private JMenuItem jmiExit = new JMenuItem("Exit");
+    Container cp;
     public Frame(){
         exe();
     }
@@ -8,5 +19,21 @@ public class Frame extends JFrame{
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setBounds(150,150,980,600);
+        this.setJMenuBar(jmb);
+        cp = this.getContentPane();
+        jmb.add(jmnuFile);
+        jmb.add(jmnuTool);
+        jmb.add(jmnuGame);
+        jmb.add(jmnuHelp);
+        jmnuFile.add(jmiOpen);
+        jmnuFile.add(jmiClose);
+        jmnuFile.add(jmiExit);
+
+        jmiExit.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
